717ee49d88cd5e2b219e4884796f39a217b287ae
in03 <in03@users.noreply.github.com>
2022-10-20 16:04:24 +1000

    feat: Add debug logging

--- resolve-slack-notifier.py
+++ resolve-slack-notifier.py
@@ -15,10 +15,14 @@ from slack_sdk.errors import SlackApiError
 dotenv.load_dotenv(verbose=True)
 
 TOKEN = os.getenv('RENDERBOT_SLACK_TOKEN')
+assert TOKEN
 CHANNEL_NAME = os.getenv('RENDERBOT_CHANNEL_NAME')
+assert CHANNEL_NAME
 
 resolve = davinci.Resolve()
 client = WebClient(token=TOKEN)
+
+logger.debug(f"Channel name: {CHANNEL_NAME}")
 
 def get_job_info(project, job_id):
     jobList = project.GetRenderJobList()
@@ -41,12 +45,14 @@ def main():
     try:
         
         project = resolve.project
+        logger.debug(f"Project name: {resolve.project.name}")
         detailed_status = project.render_status(job)
         message = f"Slack Message sent by hostname: {socket.gethostname()}, project name: {project.name}\n"
         message += f"Message initiated by: {sys.argv[0]}\n"
         message += f"job id: {job}, job status: {status}, error (if any) {error}\n"
         message += f"Detailed job status: {str(detailed_status)}"
         message += f"Job Details: {str(get_job_info(project, job))}\n"
+        logger.debug(f"Sending message: {message}")
         notify_slack(message)
         
     except Exception as e:
